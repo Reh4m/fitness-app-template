c8d69c20d14200be66cc1b8076d15deab22c43a4
emmarp <ruiizemmanuel@gmail.com>
2025-09-24 22:20:24 -0600

    Project setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FitnessApp());
+}
+
+class FitnessApp extends StatelessWidget {
+  const FitnessApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, title: 'Fitness App');
+  }
+}
