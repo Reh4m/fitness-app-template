a33c9fd2e6483b69ce6117e012a8635b51ebaefe
emmarp <ruiizemmanuel@gmail.com>
2025-09-30 00:35:36 -0600

    Feat (ColorPallete): Add color color variations

--- lib/src/themes/color_palette.dart
+++ lib/src/themes/color_palette.dart
@@ -3,8 +3,28 @@ import 'package:flutter/material.dart';
 class ColorPalette {
   // App Colors
   static Color get tropicalIndigo => const Color(0xFFA48AED);
+  static Color get tropicalIndigoLight => const Color(0xFFEAECFF);
+
   static Color get imperialRed => const Color(0xFFED4747);
+  static Color get imperialRedLight => const Color(0xFFFFEBEB);
+  static Color get imperialRedSecondary => const Color(0xFFF9B9B9);
+  static Color get imperialRedTertiary => const Color(0xFFF6CFCF);
+
+  static Color get russianViolet => const Color(0xFF3D275A);
+  static Color get russianVioletLight => const Color(0xFFEFE2FF);
+  static Color get russianVioletSecondary => const Color(0xFFD6BBF8);
+  static Color get russianVioletTerteary => const Color(0xFFF5EDFF);
+
+  static Color get charcoal => const Color(0xFF2B4855);
+  static Color get charcoalLight => const Color(0xFFD8E6EC);
+  static Color get charcoalSecondary => const Color(0xFFAED1E0);
+
   static Color get hunyadiYellow => const Color(0xFFFCC46F);
+  static Color get hunyadiYellowDark => const Color(0xFF865A19);
+  static Color get hunyadiYellowLight => const Color(0xFFFFE8C6);
+  static Color get hunyadiYellowSecondary => const Color(0xFFF8D39D);
+  static Color get hunyadiYellowTertiary => const Color(0xFFFFEDD1);
+
   static Color get skyblue => const Color(0xFF95CCE3);
   // Grey Scale
   static const Color greyDarken1 = Color(0xFF8B8F92);
