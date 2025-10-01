085ea130337157b5d71fe6523033ee689d7711cc
emmarp <ruiizemmanuel@gmail.com>
2025-09-30 23:45:34 -0600

    Feat (Main): Define initial routes for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+import 'package:fitness_app/src/screens/analytics/index.dart';
+import 'package:fitness_app/src/screens/explore/index.dart';
+import 'package:fitness_app/src/screens/home/index.dart';
+import 'package:fitness_app/src/screens/onboarding_screen.dart';
+import 'package:fitness_app/src/screens/root_screen.dart';
+import 'package:fitness_app/src/screens/workout/index.dart';
 import 'package:fitness_app/src/themes/index.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +21,15 @@ class FitnessApp extends StatelessWidget {
       title: 'Fitness App',
       theme: AppTheme.light,
       themeMode: ThemeMode.light,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const RootScreen(),
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/explore': (context) => const ExploreScreen(),
+        '/analytics': (context) => const AnalyticsScreen(),
+        '/workout': (context) => const WorkoutScreen(),
+      },
     );
   }
 }
